888cb0024e3ed5346f8f72f1bf4961f6fb87715a
Miguelms017 <miguelmartinez030205@gmail.com>
2025-02-04 16:02:47 -0500

    ESLint Added

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log("Miguel Angel Martinez Serrato");
+const myName = "Miguel Angel Martinez Serrato";
+let myDay= "My birthday day";
+
+myDay = "Wednesday";
+
+const message = `Hello, My name is ${myName} and my special day this year is ${myDay}!`;
+
+console.log(message);
